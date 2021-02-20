69fe3e8e55f65f7061455355c770e5d85e396672
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-02-19 22:14:54 -0500

    Implemented memoization

--- programs/recursive/harmonicSumRecursively.py
+++ programs/recursive/harmonicSumRecursively.py
@@ -1,9 +1,20 @@
 
+cache = dict()
+
+cache[0] = 1
+cache[1] = 1.5
+
 def harmonic(n):
-    if n == 1:
-        return 1
-    else:
-        return (1/n) + harmonic(n-1)
+    if n in cache:
+        return cache[n]
+    cache[n] = (1/n) + harmonic(n-1)
+    return cache[n]
 
 while True:
-    print(harmonic(int(input())))
+    x = int(input())
+    while (x - (max(cache.keys()))) > 950:
+        y = max(cache.keys()) + 950
+        harmonic(y)
+    print(harmonic(x))
+
+# CAUTION - THESE PROGRAMS CAN BE EXTREMELY RESOURCE INTENSIVE AND HANG YOUR COMPUTER. USE AT YOUR OWN RISK
