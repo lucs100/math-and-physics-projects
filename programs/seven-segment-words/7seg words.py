65a5aaf28dcf3a2b993fc5d5d471fc64e02cf3fb
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-05-30 23:32:46 -0400

    Now loops through all exclusion sets

--- programs/seven-segment-words/7seg words.py
+++ programs/seven-segment-words/7seg words.py
@@ -1,10 +1,10 @@
 # Not exactly a math and physics problem, this is a Python solution to the problem posed in this video:
-# https://www.youtube.com/watch?v=zp4BMR88260
+# https://www.youtube.com/watch?v=zp4BMR88260s
 
 import re
 
 def loadWords():
-    with open('words_alpha.txt') as wordFile:
+    with open('programs/seven-segment-words/words_alpha.txt') as wordFile:
         validWords = set(wordFile.read().split())
 
     return validWords
@@ -16,10 +16,8 @@ okWords = {}
 checkSets = {
     1: "[gkmqvwxz]",
     2: "[gikmoqvwxz]",
-    3: "[gikmoqsvwxz]"
+    3: "[gikmoqsvwxz]",
 }
-
-mode = 3 # strictness level defined in checkSets
 
 def addToOk(word, length):
     if length in okWords:
@@ -27,15 +25,25 @@ def addToOk(word, length):
     else:
         okWords[length] = {word}
 
-def checkSeg(word):
+def checkSeg(word, mode):
     if re.search(checkSets[mode], word) == None:
         return True
     return False
 
-for word in allWords:
-    length = len(word)
-    if length > 8: #arbitrary, help cut down search time
-        if checkSeg(word):
-            addToOk(word, length)
+def formatWords(wordSet):
+    wordSet = wordSet.replace('{', "").replace('}', "").replace('\'', "")
+    return wordSet
+    
 
-print(okWords[max(okWords)])
+for n in range(1, len(checkSets)+1):
+    for word in allWords:
+        length = len(word)
+        if length > 8: #arbitrary, help cut down search time
+            if checkSeg(word, n):
+                addToOk(word, length)
+
+    ml = max(okWords)
+    valids = formatWords(str(okWords[ml]))
+    print(f"Exclusion set: {checkSets[n]}\tMaximum length: {str(ml)}\tValid words of maximum length: {valids}")
+    okWords = {}
+
